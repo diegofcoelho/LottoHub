735c6e2b5b53c55efd1cd380d9204d348fb80425
Diego F Coêlho <diegofcoelho@users.noreply.github.com.com>
2019-04-20 13:19:15 -0300

    #345
    Size Problems continues

--- LottoWebCore/admin.py
+++ LottoWebCore/admin.py
@@ -37,9 +37,9 @@ class TicketAdmin(admin.ModelAdmin):
 @admin.register(MiddleMan)
 class MiddleManAdmin(admin.ModelAdmin):
     ordering = ['id']
-    list_display = ['id', 'raffle', 'name', 'analysed', 'directory']
+    list_display = ['id', 'name', 'analysed', 'directory']
     list_filter = ['name', 'analysed']
-    search_fields = ['name', 'raffle', 'directory', 'analysed']
+    search_fields = ['name', 'directory', 'analysed']
     readonly_fields = ['id']
 
 
--- LottoWebCore/models.py
+++ LottoWebCore/models.py
@@ -72,14 +72,14 @@ class MiddleMan(models.Model):
     phone = models.CharField(max_length=500, null=True)
     email = models.CharField(max_length=500, null=True)
     analysed = models.BooleanField(default=False)
-    raffle = models.ForeignKey(Raffle, on_delete=models.CASCADE, null=True)
+    #raffle = models.ForeignKey(Raffle, on_delete=models.CASCADE, null=True)
     id = models.CharField(max_length=100, default=create_hash, unique=True, primary_key=True)
     directory = models.ForeignKey(StudentDirectory, on_delete=models.CASCADE, null=True)
 
     readonly_fields = ('id',)
 
     class Meta:
-        unique_together = ["id", "directory", "raffle"]
+        unique_together = ["id", "directory"] #, "raffle"]
         verbose_name = "Colaborador"
         verbose_name_plural = "Colaboradores"
 
