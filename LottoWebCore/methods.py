5a4e779ab615657eeb9fab4cf69773424c8d994f
Diego F Coêlho <diegofcoelho@users.noreply.github.com.com>
2019-04-20 15:30:57 -0300

    #0.01
    Working Version

--- LottoWebCore/methods.py
+++ LottoWebCore/methods.py
@@ -6,7 +6,7 @@ def create_hash():
     ticket_hash = hashlib.sha1()
     ticket_hash.update(str(time.time()).encode('utf-8'))
     # return ticket_hash.hexdigest()[:-10]
-    return ticket_hash.hexdigest()[:-10]
+    return ticket_hash.hexdigest()[:10]
 
 
 def create_tickets():
